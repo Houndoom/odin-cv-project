3de980e84b3dbcd8e10695a917a3d21155889fc2
Houndoom <therealhoundoom@gmail.com>
2022-08-06 21:55:35 +0800

    Add edit function for profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,26 @@
+import { editableInputTypes } from '@testing-library/user-event/dist/utils';
 import React, { Component } from 'react';
+import EditableField from './EditableField';
+import EditIcon from './EditIcon';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      profile: {
+        text: 'Provide a brief description of yourself here.',
+        edit: false
+      }
+    };
+  }
+
   render () {
+    const { profile } = this.state;
     return (
-      <div className='profile-text'>
-        Analytically strong mathematician with extensive experience in data manipulation using various programming languages. Creative problem solver who leverages on coding to refine processes.
+      <div className='profile-text'>       
+        <EditableField edit={profile.edit} for='profile' this={this} name='profile' value={profile.text} />
+        <EditIcon edit={profile.edit} for='profile' this={this} />
       </div>
     );
   }
